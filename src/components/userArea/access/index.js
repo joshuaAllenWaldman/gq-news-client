90995736d6341b837744971ac812a968bbb27d20
joshuaWaldman <joshuaallenwaldman@gmail.com>
2021-06-28 16:08:01 -0700

    begins graphql connection

--- src/components/userArea/access/index.js
+++ src/components/userArea/access/index.js
@@ -1,13 +1,86 @@
 import React, { useState, useEffect } from 'react'
+import { Form, Button, Row, Col, Alert } from 'react-bootstrap'
+import { useFormik } from 'formik';
+import * as Yup from 'yup'
+
 
 const UserAccess = () => {
-
-
+  const [type, setType] = useState(true);
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid Email').required('Email Required'),
+      password: Yup.string().min(5, 'Must be more than 5 char').required('Password Required')
+    }),
+    onSubmit: values => {
+      console.log(values)
+    }
+  })
+  const switchTypeHandler = () => {
+    setType(!type)
+  }
 
   return (
-    <div>
-      access
-    </div>
+    <>
+      <Form onSubmit={formik.handleSubmit}>
+        <Row className="mb-4" >
+          <Col>
+            <h1>Sign in / Register</h1>
+          </Col>
+        </Row>
+        <Form.Group>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control 
+            type="text"
+            placeholder="Enter your email"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <Alert variant="danger"> 
+              {formik.errors.email}
+            </Alert>
+          ) : null}
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control 
+            type="password"
+            placeholder="Enter Password"
+            id="password"
+            name="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <Alert variant="danger"> 
+              {formik.errors.password}
+            </Alert>
+          ) : null}
+        </Form.Group>
+        {
+          type ?
+          <Button variant="primary" type="submit" >Sign In</Button>
+          :
+          <Button variant="primary" type="submit" >Register</Button>
+        }
+        <Button 
+        variant="secondary"
+        className="ml-2"
+        onClick={switchTypeHandler}
+        >
+          Already {type? "Signed in ": "Registered"}? Click Here
+        </Button>
+
+      </Form>
+    </>
   )
 }
 
